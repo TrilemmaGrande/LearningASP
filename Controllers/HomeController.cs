5fff34ddd4333de3ac4a7f7cffa24c22a944f4b4
Psychokater <psychokater@protonmail.com>
2023-05-15 10:07:49 +0200

    Projektdateien hinzufügen.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using LearningASP.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            string test = "";
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (i + j == 10)
+                    {
+                        test+='x';
+                    }
+                    else if (i == j & i > 0)
+                    {
+                        test += 'x';
+                    }
+                    else
+                    {
+                        test += ' ';
+                    }
+                }
+                test += '\n';
+            }
+            return test;
+        }
+    }
+}
