87b11dbdebda3e57f4017df99ee67277e3d65597
Trilemma <psychokater@protonmail.com>
2023-05-16 12:03:53 +0200

    init

---
+++ Beispiel.Party/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Beispiel.Party.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Beispiel.Party.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+        public IActionResult ListResponses()
+        {
+            return View(Repository.GetResponses()
+                                    .Where(r => r.WillAttend == true).ToList());
+        }
+    }
+}
---
+++ Beispiel.Party/Models/Repository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Beispiel.Party.Models
+{
+    public static class Repository
+    {
+        private static List<GuestResponse> responses = new List<GuestResponse>();
+        public static List<GuestResponse> GetResponses()
+        {
+            return responses;
+        }
+        public static void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+        }
+    }
+}
